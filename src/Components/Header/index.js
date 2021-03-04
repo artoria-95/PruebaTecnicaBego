2ff2e83a65915f5ebee557a899cb0dfa17831e9e
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-03-03 22:17:28 -0300

    orden

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 import { RiArrowDropLeftLine } from "react-icons/ri";
-import './headstyle.css';
+import "./headstyle.css";
 
-
-export default function Header(){
-
-  return(
+export default function Header() {
+  return (
     <div>
-     <div className="contenedor">
-       <span className="arrow"><RiArrowDropLeftLine/></span>
-       <span className="tasks">Tasks</span>
-       <div className="imagen">
-       <img src="https://projects.invisionapp.com/assets/21135679/987684212/D4B84B9D84CC26025320A2114CF9C54C3BF4ED5A837B4A1F61AC785215FC019E/preview?w=758&cacheVersion=1" alt="headback"/>
-       </div>
-       <div className="create">CREATE +</div>
-          <div className="nav">
-        <ul>
-          <li className="all">All</li>
-          <li className="other1">In Progress</li>
-          <li className="other2">Started</li>
-          <li className="other3">Completed</li>
-        </ul>
+      <div className="contenedor">
+        <span className="arrow">
+          <RiArrowDropLeftLine />
+        </span>
+        <span className="tasks">Tasks</span>
+        <div className="imagen">
+          <img
+            src="https://projects.invisionapp.com/assets/21135679/987684212/D4B84B9D84CC26025320A2114CF9C54C3BF4ED5A837B4A1F61AC785215FC019E/preview?w=758&cacheVersion=1"
+            alt="headback"
+          />
+        </div>
+        <div className="create">CREATE +</div>
+        <div className="nav">
+          <ul>
+            <li className="all">All</li>
+            <li className="other1">In Progress</li>
+            <li className="other2">Started</li>
+            <li className="other3">Completed</li>
+          </ul>
+        </div>
       </div>
-       </div>
     </div>
-  )
+  );
 }
