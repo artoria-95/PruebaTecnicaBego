8aba026daf6d6b8d64d89c13c90d160e890fd577
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-03-03 23:03:14 -0300

    october line

--- src/Components/Months/index.js
+++ src/Components/Months/index.js
@@ -18,8 +18,10 @@ export default function Months() {
         <div className="cash">
           <span className="pesos">$</span> <span className="num">13,543</span>
         </div>
-        <img src="https://projects.invisionapp.com/assets/21135679/987684215/5059ECD73249DBC6C834CD9C00351433677EE7DD4AC5FFEA4580F94854938D1F/preview?w=758&cacheVersion=1" />
-
+        <img src="https://projects.invisionapp.com/assets/21135679/987684215/5059ECD73249DBC6C834CD9C00351433677EE7DD4AC5FFEA4580F94854938D1F/preview?w=758&cacheVersion=1" className="wave"/>
+        
+        <div className="oct-dot"/>
+        <div className="oct-line"/>
         <ul className="meses">
           <li className="mes">Jun</li>
           <li className="mes">Jul</li>
@@ -28,6 +30,7 @@ export default function Months() {
           <li className="oct">Oct</li>
           <li className="mes">Nov</li>
         </ul>
+      
       </div>
       <img
         src="https://projects.invisionapp.com/assets/21135679/987684213/17AC41C05CB672FE00E3DE96AD13B4C0D4F5EBCA7D2AC5B126D57DBD57BFDC18/preview?w=758&cacheVersion=1"
